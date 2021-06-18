fdff74230db2ab057b4528cf2f618eaf0a08bcda
Grigorios Kaframanis <gkaframanis@gmail.com>
2021-06-18 16:43:41 +0300

    Adding comments to the code

--- pdf_watermark.py
+++ pdf_watermark.py
@@ -15,25 +15,33 @@ def watermark_pdfs(pdf_files, watermark):
         with open(pdf_file, "rb") as pdf_file:
             # Reading content of the pdf file.
             pdf_reader = PyPDF2.PdfFileReader(pdf_file)
-            # Reading the content of the watermark file
             with open(watermark, "rb") as watermark_file:
+                # Reading the content of the watermark file
                 watermark_reader = PyPDF2.PdfFileReader(watermark_file)
 
                 page_number = 0
 
+                # Get the first and only page of the watermark file.
                 watermark_page = watermark_reader.getPage(page_number)
-                    
+                
+                # Creating a PdfFileWriter object.
                 pdf_writer = PyPDF2.PdfFileWriter()
 
                 while page_number < pdf_reader.getNumPages():
+                    # Get each page of each pdf file
                     pdf_page = pdf_reader.getPage(page_number)
 
+                    # Merge the watermark and the pdf page.
                     pdf_page.mergePage(watermark_page)
 
+                    # Add the page to write it.
                     pdf_writer.addPage(pdf_page)
 
                     with open(f"watermarked_{pdf_file.name}", mode="wb") as watermarked_pdf_file:
+                        # Write the page to a newly created pdf file.
                         pdf_writer.write(watermarked_pdf_file)
+                        
+                    # Move to the next page of the pdf file.
                     page_number += 1
 
 
